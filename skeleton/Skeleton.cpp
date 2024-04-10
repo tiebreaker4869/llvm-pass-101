9fc3d41f93f1a827815d8cbc53438cf4cd7dea0c
Lifan Sun <lifansun1412@gmail.com>
2024-04-10 21:55:45 +0800

    project init

---
+++ skeleton/Skeleton.cpp
@@ -0,0 +1,34 @@
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+
+struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
+    PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+        for (auto &F : M) {
+            errs() << "I saw a function called " << F.getName() << "!\n";
+        }
+        return PreservedAnalyses::all();
+    };
+};
+
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return {
+        .APIVersion = LLVM_PLUGIN_API_VERSION,
+        .PluginName = "Skeleton pass",
+        .PluginVersion = "v0.1",
+        .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineStartEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel Level) {
+                    MPM.addPass(SkeletonPass());
+                });
+        }
+    };
+}
